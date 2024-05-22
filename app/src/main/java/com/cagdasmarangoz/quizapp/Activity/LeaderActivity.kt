b6c26f1a047bccadb31827936101f27c9a68a4f6
Cagdas MARNAGOZ <marangoz.1697@gmail.com>
2024-05-22 15:48:28 +0300

    leader activity uı first design

--- app/src/main/java/com/cagdasmarangoz/quizapp/Activity/LeaderActivity.kt
+++ app/src/main/java/com/cagdasmarangoz/quizapp/Activity/LeaderActivity.kt
@@ -1,11 +1,12 @@
-package com.cagdasmarangoz.quizapp
+package com.cagdasmarangoz.quizapp.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cagdasmarangoz.quizapp.R
 
-class MainActivity : AppCompatActivity() {
+class LeaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_leader)
     }
 }
---
+++ app/src/main/java/com/cagdasmarangoz/quizapp/Activity/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.cagdasmarangoz.quizapp.Activity
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.cagdasmarangoz.quizapp.R
+import com.cagdasmarangoz.quizapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        binding.apply {
+            singleBtn.setOnClickListener {
+
+            }
+            bottomMenuId.setItemSelected(R.id.home)
+            bottomMenuId.setOnItemSelectedListener {
+                if (it == R.id.board) {
+
+                }
+            }
+        }
+    }
+}
