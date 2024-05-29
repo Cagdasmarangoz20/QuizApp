44131ce8ab44c8a384d936a7232b4c96ab2c776d
Cagdas MARNAGOZ <marangoz.1697@gmail.com>
2024-05-29 17:44:19 +0300

    Question Activity UI design making

---
+++ app/src/main/java/com/cagdasmarangoz/quizapp/activity/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.cagdasmarangoz.quizapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cagdasmarangoz.quizapp.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
